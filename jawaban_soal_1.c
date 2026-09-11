14e4fa3938ca1899928ed13e349e3b3efd38163b
ixvenn <fabiantirtalines@gmail.com>
2026-09-11 19:09:05 +0800

    hidupsolo

--- jawaban_soal_1.c
+++ jawaban_soal_1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+    int a, b, c;
+
+    scanf("%i", &a);
+    scanf("%i", &b);
+    scanf("%i", &c);
+    
+    if (a + b == c) {
+        printf("+");
+    }
+    else if (a - b == c) {
+        printf("-");
+    }
+    else if (a * b == c) {
+        printf("*");
+    }
+    return 0;
+}
